f3b62dc53935e7e01c22a93110cca1002d055be9
Zoe Jane <dadac123@gmail.com>
2017-07-01 14:20:42 +0800

    add simple gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
+ });
+
+gulp.task('html', function() {
+	console.log("Imagine something useful being done to your HTML here.")
+});
+
+gulp.task('styles', function() {
+	console.log("Imagine Sass or PostCSS tasks running here.")
+});
+
+gulp.task('watch', function() {
+	watch('./app/index.html', function (){
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+});
