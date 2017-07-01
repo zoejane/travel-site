891d088dc8ee2043261140b168a5fbb9ca458a59
Zoe Jane <dadac123@gmail.com>
2017-07-01 15:09:42 +0800

    add more gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -10,7 +14,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-	console.log("Imagine Sass or PostCSS tasks running here.")
+	return gulp.src('./app/assets/styles/styles.css')
+	  .pipe(postcss([cssvars, nested, autoprefixer]))
+	  .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
